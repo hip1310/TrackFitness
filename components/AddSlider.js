01e5e07b42bed54a6ba14236fa86c8149c2b6919
hip1310 <harshpatel131090@gmail.com>
2018-01-03 08:18:35 -0500

    Add functionality to log activities for particular day
    If user has already logged activities for particular day, then show specific message.

---
+++ components/AddSlider.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { View, Text, Slider } from 'react-native'
+
+export default function AddSlider ({ max, unit, step, value, onChange }) {
+  return (
+    <View>
+      <Slider
+        step={step}
+        value={value}
+        maximumValue={max}
+        minimumValue={0}
+        onValueChange={onChange}/>
+      <View>
+        <Text>{value}</Text>
+        <Text>{unit}</Text>
+      </View>
+    </View>
+  )
+}
